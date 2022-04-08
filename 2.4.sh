4b4ad9a361be7b8613d75909534a26ff5afdb45a
DOTOLI FRANCESCO <FRANCESCO.DOTOLI@cz.lan>
2022-04-08 16:22:43 +0200

    c

--- 2.4.sh
+++ 2.4.sh
@@ -1,1 +1,1 @@
-pip3 install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp35-none-linux_aarch64.whl
+pip3 install https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_aarch64.whl
